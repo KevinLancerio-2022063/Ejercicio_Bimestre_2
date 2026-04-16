2176ae765507021410d6ebd0d9db7a87e87c3900
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-04-15 23:22:21 -0600

    feat: Se ha agregado la clase LoginServiceImplements

---
+++ Ejercicio/Ejercicio/src/main/java/com/kevinlancerio/Ejercicio/Service/LoginServiceImplements.java
@@ -0,0 +1,53 @@
+package com.kevinlancerio.Ejercicio.Service;
+
+import com.kevinlancerio.Ejercicio.Entity.Login;
+import com.kevinlancerio.Ejercicio.Repository.LoginRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LoginServiceImplements implements LoginService {
+
+    @Autowired
+    private LoginRepository repo;
+
+    @Override
+    public Login registrar(String usuario, String password) {
+
+        if (repo.findByUsuario(usuario) != null) {
+            return null;
+        }
+
+        Login l = new Login();
+        l.setUsuario(usuario);
+        l.setPassword(password);
+
+        return repo.save(l);
+    }
+
+    @Override
+    public Login login(String usuario, String password) {
+
+        Login l = repo.findByUsuario(usuario);
+
+        if (l != null && l.getPassword().equals(password)) {
+            return l;
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Login> listar() {
+        return repo.findAll();
+    }
+
+    @Override
+    public void eliminar(int id) {
+        repo.deleteById(id);
+    }
+
+}
+
