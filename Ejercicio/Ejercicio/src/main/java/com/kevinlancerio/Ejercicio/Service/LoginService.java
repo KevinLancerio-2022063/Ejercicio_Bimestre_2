7ae103651f537fa10d7643e6bdf696c23d46b4d1
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-04-17 22:06:00 -0600

    feat: Se han agregado el saveLogin y buscarPorUsuario en el LoginService

--- Ejercicio/Ejercicio/src/main/java/com/kevinlancerio/Ejercicio/Service/LoginService.java
+++ Ejercicio/Ejercicio/src/main/java/com/kevinlancerio/Ejercicio/Service/LoginService.java
@@ -1,6 +1,8 @@
 package com.kevinlancerio.Ejercicio.Service;
 
 import com.kevinlancerio.Ejercicio.Entity.Login;
+import com.kevinlancerio.Ejercicio.Entity.Producto;
+import com.kevinlancerio.Ejercicio.Entity.Usuario;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,4 +14,6 @@ public interface LoginService {
     Login login(String usuario, String password);
     List<Login> listar();
     void eliminar (int id);
+    Login saveLogin (Login login) throws RuntimeException;
+    Login buscarPorUsuario (String usuario);
 }
