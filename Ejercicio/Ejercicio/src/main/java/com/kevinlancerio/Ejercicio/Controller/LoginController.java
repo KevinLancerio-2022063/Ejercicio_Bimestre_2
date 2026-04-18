8964614b578ef8e60c6f706deb70d0c525de1d16
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-04-17 22:04:56 -0600

    feat: Se han agregado los métodos para los roles y subir imágenes en el Rol USER

--- Ejercicio/Ejercicio/src/main/java/com/kevinlancerio/Ejercicio/Controller/LoginController.java
+++ Ejercicio/Ejercicio/src/main/java/com/kevinlancerio/Ejercicio/Controller/LoginController.java
@@ -5,11 +5,17 @@ import com.kevinlancerio.Ejercicio.Entity.Login;
 import com.kevinlancerio.Ejercicio.Service.LoginService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
 
 
 @Controller
@@ -26,6 +32,7 @@ public class LoginController {
     @PostMapping("/login")
     public String validar(@RequestParam String usuario,
                           @RequestParam String password,
+                          @RequestParam String rol,
                           HttpSession session,
                           Model model) {
 
@@ -33,14 +40,29 @@ public class LoginController {
 
         if (l != null) {
 
-            session.setAttribute("usuario", l.getUsuario());
-            session.setAttribute("rol", l.getRol());
+            if (l.getRol().equals(rol)) {
 
-            return "redirect:/menu";
+                session.setAttribute("usuario", l.getUsuario());
+                session.setAttribute("rol", l.getRol());
+
+                if ("ADMIN".equals(rol)) {
+                    return "redirect:/menu";
+                } else if ("USER".equals(rol)) {
+                    return "redirect:/menu";
+
+                }
+
+            } else {
+                model.addAttribute("Error", "Acceso denegado, solo puede ingresar un ADMIN");
+                return "loginInicio";
+            }
+
+
         } else {
             model.addAttribute("error", "Credenciales incorrectas");
-            return "loginInicio";
         }
+        return "loginInicio";
+
     }
 
     @GetMapping("/registro")
@@ -52,6 +74,13 @@ public class LoginController {
     public String guardar(@RequestParam String usuario,
                           @RequestParam String password,
                           Model model) {
+
+        Login log = new Login();
+        log.setUsuario(usuario);
+        log.setPassword(password);
+        log.setRol("USER");
+
+        service.saveLogin(log);
 
         Login l = service.registrar(usuario, password);
 
@@ -66,6 +95,55 @@ public class LoginController {
     @GetMapping("/menu")
     public String menu() {
         return "menu";
+    }   
+
+    @PostMapping("/subirImagen")
+    public String subirImagen(@RequestParam("file") MultipartFile file,
+                              HttpSession session) {
+
+        try {
+            String usuario = (String) session.getAttribute("usuario");
+            Login l = service.buscarPorUsuario(usuario);
+
+            if (!file.isEmpty()) {
+                l.setImagen(file.getBytes());
+                service.saveLogin(l);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/menu";
+    }
+
+    @GetMapping("/loginInicio/imagen")
+    @ResponseBody
+    public ResponseEntity<byte[]> mostrarImagen(HttpSession session) {
+
+        try {
+            String usuario = (String) session.getAttribute("usuario");
+            Login user = service.buscarPorUsuario(usuario);
+
+            if (user.getImagen() != null) {
+                return ResponseEntity
+                        .ok()
+                        .header("Content-Type", "image/jpeg")
+                        .body(user.getImagen());
+            }
+
+            InputStream is = new ClassPathResource("static/images/UsuarioPerfil.webp").getInputStream();
+            byte[] img = is.readAllBytes();
+
+            return ResponseEntity
+                    .ok()
+                    .header("Content-Type", "image/webp")
+                    .body(img);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.notFound().build();
+        }
     }
 
     // Cerrar sesión
